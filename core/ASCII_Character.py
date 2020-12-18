fcfc07960fa6ef0225bbbc2265f51bd6df85461c
Caroline <cj.jin@hotmail.com>
2020-12-18 14:54:42 -0500

    Added files for implementation

--- core/ASCII_Character.py
+++ core/ASCII_Character.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-class ASCII_Art_Character:
+class ASCII_Character:
 
     '''
     field:
@@ -19,18 +19,24 @@ class ASCII_Art_Character:
     '''
     Returns the ASCII charater of this
     '''
-    def get_character():
+    def get_character(self):
         return self.character
 
     '''
     Returns the numpy array representation of the ASCII character
     '''
-    def get_character_pixel_array():
-        return None
+    def get_character_pixel_array(self):
+        im = Image.new('1', (6, 11), color = 1)
+        draw = ImageDraw.Draw(im)
+        draw.text((0, 0), self.character, font = ImageFont.load_default())
+        return np.array(im.getdata()).reshape((11,6))
 
     '''
     Returns the intensity of the character 
     '''
-    def get_intensity():
-        return 0
+    def get_intensity(self):
+        arr = self.get_character_pixel_array()
+        shape = arr.shape
+        numChars = shape[0] * shape[1]
+        return (numChars - sum(sum(arr)))/numChars
 
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name = "ASCII_Art_Cat_Dog", packages = find_packages())
